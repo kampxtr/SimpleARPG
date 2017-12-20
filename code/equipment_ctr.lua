ba97e6e4e9fcaaabee75f85a40dc8630a7530ace
kampxtr <aa1157006451@gmail.com>
2017-12-20 13:06:41 +0800

    Update equipment_ctr.lua

--- code/equipment_ctr.lua
+++ code/equipment_ctr.lua
@@ -1,30 +1,25 @@
 
 component = "equipment_ctr"
 itemData = {}
+local onGround = false
 
-
-function start()
-end
-
-function update()
-	local transform = this.entity.Transform
-	if(transform and transform:getAbsPos().y <= 2) then
-		this.entity.PhysicsComponent:setMass(0)
-	end
-end
 
 function onTriggerIn(trigger, enterer)
-   if(trigger == this.entity and (enterer and enterer:getName() == player_entity_name)) then 
+   if(trigger == this.entity) then
+	if(enterer and enterer:getName() == player_entity_name) then 
 		-- enterer is warrior
 		local game = thisScene():findEntityByName("game")
 		if(game.inventory_ctr) then
 			local item_ctr = game.inventory_ctr
 			if(item_ctr) then
 				item_ctr.addItem(itemData)
-				Event:trigger(game_event.ITEM_BE_PICK, { entity = this.entity })
-				thisScene():removeEntity(this.entity)
 			end
 		end
+		Event:trigger(game_event.ITEM_BE_PICK, { entity = this.entity })
+		thisScene():removeEntity(this.entity)
+	elseif(enterer and enterer:getName() == "terrain") then 
+		this.entity.Rigidbody:setMass(0)
+	end
    end
 end
 
