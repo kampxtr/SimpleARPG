970906fd87af4b0bae083cd3eda4a92cedab121e
kampxtr <aa1157006451@gmail.com>
2017-12-23 19:04:03 +0800

    Update warrior_ctr_attack.lua

--- code/warrior_ctr_attack.lua
+++ code/warrior_ctr_attack.lua
@@ -1,19 +1,26 @@
 
 component = "warrior_ctr_attack"
 local spawner = {}
+local animAttack = -1
+local animator = {}
 
 function start()
 	local cmp = this.entity.warrior_ctr_attack
 	spawner = thisScene():findEntityByName("spawner")
-	this.entity.Animator:setAnimation("./Animations/Warrior_Idle.anim")
+	animator = this.entity.Animator
+	if(animator) then
+		animAttack = animator:getPropertyIndex("attack")
+	end
 end
 
-function update()
-end
 
 function onInput(evt) 
    if(evt.type == FW.InputEvent.EventType.KeyPressed) then
        if(FW.Keyboard.Key.X == evt.key.code) then
+	      if(animator) then
+		animator:setFloatProperty(animAttack, 1)
+	      end
+
 	      local spawnercmp = spawner.spawner
 		  if(spawnercmp) then
 		    local mob = spawnercmp.mobList[1]
@@ -22,7 +29,7 @@ function onInput(evt)
 				if(mobhealth)  then mobhealth.beAttack(50) end
 			end
 		  end
-	   end
+	end
    end
 end
 
